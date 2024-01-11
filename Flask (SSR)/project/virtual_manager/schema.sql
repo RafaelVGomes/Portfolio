6d4bd9ef0be926c9a5c0eee010075a08b2c18bf9
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-01-11 14:31:22 -0300

    updating

--- Flask (SSR)/project/virtual_manager/schema.sql
+++ Flask (SSR)/project/virtual_manager/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     hash TEXT NOT NULL,
@@ -7,9 +7,10 @@ CREATE TABLE IF NOT EXISTS users (
     is_admin INTEGER DEFAULT 0,
     cash NUMERIC DEFAULT 0.00
 );
+CREATE INDEX idx_users ON users (id, username, is_active);
 
 
-CREATE TABLE IF NOT EXISTS items (
+CREATE TABLE items (
     id INTEGER PRIMARY KEY,
     item_name TEXT NOT NULL UNIQUE,
     amount NUMERIC NOT NULL,
@@ -19,10 +20,10 @@ CREATE TABLE IF NOT EXISTS items (
     quantity_alert INTEGER NOT NULL,
     measure TEXT NOT NULL
 );
-CREATE INDEX IF NOT EXISTS idx_items ON items (item_name);
+CREATE INDEX idx_items ON items (id, item_name, is_product);
 
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE products (
     id INTEGER PRIMARY KEY,
     product_name TEXT NOT NULL UNIQUE,
     amount NUMERIC NOT NULL,
@@ -31,21 +32,20 @@ CREATE TABLE IF NOT EXISTS products (
     price NUMERIC NOT NULL,
     has_recipe INTEGER NOT NULL
 );
-CREATE INDEX IF NOT EXISTS idx_product ON products (product_name);
+CREATE INDEX idx_product ON products (id, product_name);
 
 
-CREATE TABLE IF NOT EXISTS recipes (
-    product_id INTEGER NOT NULL,
+CREATE TABLE recipes (
+    product_id INTEGER PRIMARY KEY,
     item_id INTEGER NOT NULL,
-    item_name TEXT NOT NULL,
     amount NUMERIC NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES items (id) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE
 );
-CREATE INDEX IF NOT EXISTS idx_recipes ON recipes (product_id);
+CREATE INDEX idx_recipes ON recipes (product_id, item_id);
 
 
-CREATE TABLE IF NOT EXISTS historic (
+CREATE TABLE historic (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     date TEXT DEFAULT CURRENT_TIMESTAMP,
@@ -54,4 +54,4 @@ CREATE TABLE IF NOT EXISTS historic (
     amount INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE INDEX IF NOT EXISTS idx_historic ON historic (user_id, trade);
+CREATE INDEX idx_historic ON historic (user_id, date, trade);
