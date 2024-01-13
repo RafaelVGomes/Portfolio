4c546778cd933dbca795156a730b436fcd510697
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-01-13 10:50:19 -0300

    minor changes

--- Flask (SSR)/project/virtual_manager/static/layout.js
+++ Flask (SSR)/project/virtual_manager/static/layout.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Compares if the 3rd part of the visited and <li> href link matches
-    document.querySelectorAll(".nav-link").forEach((x) => {
+    // Compares if the 3rd part of the visited link and <li> href link matches
+    const layoutNavbar = document.getElementById('layoutNavbar')
+    layoutNavbar.querySelectorAll(".nav-link").forEach((x) => {
         if (window.location.href.split('/')[3] == x.href.split('/')[3]) {
             x.classList.add('active', 'disabled')
         }
