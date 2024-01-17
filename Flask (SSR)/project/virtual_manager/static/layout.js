2302c0db5554309f8808cf347c5f322cc66444da
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-01-17 11:15:13 -0300

    navbar active link handler code improved

--- Flask (SSR)/project/virtual_manager/static/layout.js
+++ Flask (SSR)/project/virtual_manager/static/layout.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Compares if the 3rd part of the visited link and <li> href link matches
+    const url_prefix = !$('#index-tabs') ? 3 : 4
     const layoutNavbar = document.getElementById('layoutNavbar')
     layoutNavbar.querySelectorAll(".nav-link").forEach((x) => {
-        if (window.location.href.split('/')[3] == x.href.split('/')[3]) {
-            x.classList.add('active', 'disabled')
-        }
+      if (window.location.href.split('/')[url_prefix] == x.href.split('/')[url_prefix]) {
+        x.classList.add('active', 'disabled')
+      }
     })
 })
