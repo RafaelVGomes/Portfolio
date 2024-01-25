6e7e04dda5e78c424281daf550d26e84697c0647
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-01-25 07:42:00 -0300

    putting back 'dev' secret key

--- Flask (SSR)/project/virtual_manager/__init__.py
+++ Flask (SSR)/project/virtual_manager/__init__.py
@@ -7,6 +7,7 @@ from virtual_manager.helpers import usd, format_field_name
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+      SECRET_KEY='dev',
       DATABASE=os.path.join(app.instance_path, 'virtual_manager.sqlite'),
     )
 
